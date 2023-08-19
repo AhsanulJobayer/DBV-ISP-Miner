9169ba36a04792f7195b73bff6c665338cdb779e
AhsanulJobayer <ajahsanul19@gmail.com>
2023-08-20 04:20:51 +0600

    frequent k-patterns with DBV-tree structures

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ class Patternlist:
         self.frequency = 0
         self.bit_vector = 0
         self.T_P = [[] for _ in range(64000)]
+        self.children = []
     
     def position(self, t, p):
         self.T_P[t].append(p)
@@ -15,14 +16,19 @@ class Patternlist:
     
     def bit_positions(self):
         positions = []
-        position = 1
+        position = 12
         number = self.bit_vector
         while number > 0:
             if number & 1:
                 positions.append(position)
-            position += 1
+                if(position > self.finalT):
+                    self.finalT = position
+            position -= 1
             number >>= 1
         return positions
+
+    def input_T_P(self, index, value):
+        self.T_P[index].append(value)
     
     def get_frequency(self):
         return self.frequency
@@ -44,11 +50,36 @@ class Patternlist:
 
 items = {}
 
+# --------------------------------------------tree---------------------------------------------------------
+class Tree:
+    def __init__(self):
+        self.root = None
+
+    def add_root(self, root_value):
+        self.root = Patternlist(root_value)
+
+    def add_child(self, parent, child_patternlist):
+        parent.children.append(child_patternlist)
+
+    def get_nodes_at_level(self, level):
+        return self._get_nodes_at_level(self.root, level, 0)
+
+    def _get_nodes_at_level(self, current_node, target_level, current_level):
+        if current_level == target_level:
+            return [current_node]
+        
+        nodes = []
+        for child in current_node.children:
+            nodes.extend(self._get_nodes_at_level(child, target_level, current_level + 1))
+        
+        return nodes
+
+# ---------------------------------------single join -------------------------------------------------------
 def single_itemset_join(seq1, seq2):
     answer = Patternlist(seq1.sequence + " " + seq2.sequence + " -1 -2")
     answer.bit_vector = seq1.bit_vector & seq2.bit_vector
     t_value = answer.bit_positions()
-    print("item set join")
+    print("itemset join")
     for i in t_value:
         match = 0
         for value1 in seq1.T_P[i]:
@@ -67,18 +98,19 @@ def single_sequence_join(seq1, seq2):
     for i in t_value:
         match = 0
         for value1 in seq1.T_P[i]:
-            if (value1 + 1) in seq2.T_P[i]:
-                answer.T_P[i].append(seq2.T_P[i])
-                match = 1
+            for val in seq2.T_P[i]:  # Define val within this loop
+                if val > value1:
+                    answer.T_P[i].append(val)
+                    match = 1
         answer.frequency += match
 
     return answer
 
 def single_intersequence_join(seq1, seq2):
     answer = Patternlist(seq1.sequence + " -1 -2 " + seq2.sequence + " -1 -2")
-    answer.bit_vector = (seq1.bit_vector >> 1) & seq2.bit_vector
+    answer.bit_vector = ((seq1.bit_vector >> 1) & seq2.bit_vector)
     t_value = answer.bit_positions()
-    
+    print("Check bit vector if bin or dec: ", seq1.bit_vector)
     for i in t_value:
         match = 0
         for value1 in seq2.T_P[i]:
@@ -88,6 +120,54 @@ def single_intersequence_join(seq1, seq2):
 
     return answer
 
+# -------------------------------K join ---------------------------------------------------
+def k_itemmset_join(seq1, seq2):
+    items1 = seq1.sequence.split()  # Split sequence 1 into items
+    items2 = seq2.sequence.split()  # Split sequence 2 into items
+
+    # Compare items up to the second-to-last item
+    if items1[:-3] == items2[:-3]:
+        answer = Patternlist(" ".join(items1[:-3]) + " " + items1[-3] + " " + items2[-3] + " -1 -2")
+    else:
+        answer = Patternlist("none")
+        return answer
+    # answer = Patternlist(seq1.sequence + " " + seq2.sequence + " -1 -2")
+    answer.bit_vector = seq1.bit_vector & seq2.bit_vector
+    t_value = answer.bit_positions()
+    print("k-itemset join")
+    for i in t_value:
+        match = 0
+        for value1 in seq1.T_P[i]:
+            if value1 in seq2.T_P[i]:
+                answer.T_P[i].append(value1)
+                match = 1
+        answer.frequency += match
+
+    return answer
+
+def k_sequence_join(seq1, seq2):
+    items1 = seq1.sequence.split()  # Split sequence 1 into items
+    items2 = seq2.sequence.split()  # Split sequence 2 into items
+
+    # Compare items up to the second-to-last item
+    if items1[:-3] == items2[:-3]:
+        answer = Patternlist(" ".join(items1[:-3]) + " " + items1[-3] + " -1 " + items2[-3] + " -1 -2")
+    else:
+        answer = Patternlist("none")
+        return answer
+    answer.bit_vector = seq1.bit_vector & seq2.bit_vector
+    t_value = answer.bit_positions()
+    print("sequence join")
+    for i in t_value:
+        match = 0
+        for value1 in seq1.T_P[i]:
+            for val in seq2.T_P[i]:  # Define val within this loop
+                if val > value1:
+                    answer.T_P[i].append(val)
+                    match = 1
+        answer.frequency += match
+
+    return answer
 
 minsup = 4
 
@@ -149,10 +229,55 @@ for item, item_info in items.items():
     print("Sequence:", item)
     item_info.print_info()
 
-print("checking join operation")
-join = single_itemset_join(items["A"], items["B"])
-join.print_info()
-join1 = single_sequence_join(items["A"], items["B"])
-join1.print_info()
-join2 = single_intersequence_join(items["A"], items["B"])
-join2.print_info()
+# tree structure maintanance
+tree = Tree()
+null_pattern = Patternlist("null")
+tree.add_root(null_pattern)
+for one_length, freq in items.items():
+    tree.add_child(tree.root, freq)
+
+# ------level 1 operation--------
+new_items = tree.get_nodes_at_level(1)
+
+print("Checking level 1 in tree: ")
+for checks in new_items:
+    print("sequence------- ", checks.sequence, " and frequency---- ", checks.frequency)
+
+# -----level 2 operation---------
+for X in new_items:
+    for Y in new_items:
+        # itemset join
+        iJoin = single_itemset_join(X, Y)
+        if iJoin.frequency >= minsup:
+            tree.add_child(X, iJoin)
+        # sequence join
+        iJoin = single_sequence_join(X, Y)
+        if iJoin.frequency >= minsup:
+            tree.add_child(X, iJoin)
+        # inter join
+        iJoin = single_intersequence_join(X, Y)
+        if iJoin.frequency >= minsup:
+            tree.add_child(X, iJoin)
+
+new_items = tree.get_nodes_at_level(2)
+
+print("checking level 2 in tree: ")
+for checks in new_items:
+    print("sequence------- ", checks.sequence, " and frequency---- ", checks.frequency)
+
+for X in new_items:
+    for Y in new_items:
+        # #k-itemset join
+        # iJoin = k_itemmset_join(X, Y)
+        # if iJoin.frequency >= minsup:
+        #     tree.add_child(X, iJoin)
+        #K-sequence join
+        iJoin = k_sequence_join(X, Y)
+        if iJoin.frequency >= minsup:
+            tree.add_child(X, iJoin)
+
+new_items = tree.get_nodes_at_level(3)
+
+print("checking level 3 in tree: ")
+for checks in new_items:
+    print("sequence------- ", checks.sequence, " and frequency---- ", checks.frequency)
